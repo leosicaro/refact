2f3f7e227d418fa61b69e0a74d7eda4457c95363
Leonel Sicaro <leonelsicaro@gmail.com>
2024-03-15 22:42:57 +0100

    pair

---
+++ src/componentes/cabeceras.jsx
@@ -0,0 +1,10 @@
+
+export const cabecera = () => {
+    return (
+        <header className='cabecera'>
+            <a href="https://react.dev" target="_blank">
+                <img src='src/assets/cocretainc.jpg' className="logo" alt="Empresa logo" />
+            </a>
+        </header>
+    )
+}
---
+++ src/componentes/pie.jsx
@@ -0,0 +1,14 @@
+
+export const pie= () => {
+    return (
+        <footer className='footer'>
+            <ul>
+                <li>Todos los derechos reservados</li>
+                <li>Coquetas INC ©</li>
+                <li>Dirección: Simancas (Madriz)</li>
+                <li>Teléfono: 917335673 - 635444912</li>
+                <li>Email: croquetasINC@hotmail.com</li>
+            </ul>
+        </footer>
+    )
+}
